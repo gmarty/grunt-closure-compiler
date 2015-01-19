68fe660219fad31027acc5a19438233a451f14d9
abac <abac@dev.web2039.net>
2015-01-19 00:25:06 +0000

    fix

--- tasks/closure-compiler.js
+++ tasks/closure-compiler.js
@@ -59,10 +59,11 @@ module.exports = function(grunt) {
 
     if (data.externs) {
       data.externs = grunt.file.expand({cwd: data.cwd}, data.externs);
-      command += ' --externs ' + data.externs.join(' --externs ');
 
       if (!data.externs.length) {
         delete data.externs;
+      } else {
+        command += ' --externs ' + data.externs.join(' --externs ');
       }
     }
 
